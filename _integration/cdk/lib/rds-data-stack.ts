e3c31ee63741bedb0de12211ff3ae85d26a67621
ICHINOSE Shogo <shogo82148@gmail.com>
2024-11-27 02:31:42 +0900

    introduce VPC

--- _integration/cdk/lib/rds-data-stack.ts
+++ _integration/cdk/lib/rds-data-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class RdsDataStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "CdkQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      natGateways: 0,
     });
   }
 }
