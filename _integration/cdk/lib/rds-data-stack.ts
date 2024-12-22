bda84c57d6a30fd60f2da9e9fd4fcbf9ffaabf42
ICHINOSE Shogo <shogo82148@gmail.com>
2024-12-22 23:51:36 +0900

    add RDS DatabaseCluster with Aurora MySQL engine to RdsDataStack

--- _integration/cdk/lib/rds-data-stack.ts
+++ _integration/cdk/lib/rds-data-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 
 export class RdsDataStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,20 @@ export class RdsDataStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, "Vpc", {
       ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
       natGateways: 0,
+    });
+
+    const mysqlCluster = new rds.DatabaseCluster(this, "MySqlCluster", {
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_3_08_0,
+      }),
+      writer: rds.ClusterInstance.serverlessV2("writer"),
+      serverlessV2MaxCapacity: 2,
+      serverlessV2MinCapacity: 0,
+      enableDataApi: true,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      vpc,
     });
   }
 }
